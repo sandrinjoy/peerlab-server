5a78ff5a7e580e364d190b2e36e5fcbf182ae584
sandrinejoy <sandrinejoy007@gmail.com>
2022-03-18 15:38:48 +0530

    send message test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const PORT = process.env.PORT || 3000;
-
+console.log("The value of PORT is:", process.env);
 const server = express().listen(PORT, () =>
   console.log(`Listening on ${PORT}`)
 );
@@ -10,5 +10,12 @@ const wss = new Server({ server });
 
 wss.on("connection", (ws) => {
   console.log("Client connected");
+  ws.on("open", () => {
+    ws.send("welcome to peerlab server");
+  });
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+  });
+
   ws.on("close", () => console.log("Client disconnected"));
 });
