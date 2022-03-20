961e8035099855f54cffbe246554e246be16c5a1
sandrinejoy <sandrinejoy007@gmail.com>
2022-03-20 22:21:08 +0530

    test without express

--- index.js
+++ index.js
@@ -1,23 +1,70 @@
-const express = require("express");
+import { nanoid } from "nanoid";
+import { WebSocketServer } from "ws";
 const PORT = process.env.PORT || 3000;
-const server = express()
-  .get("/*", (req, res) => {
-    res.redirect("https://peerlab.vercel.app");
-  })
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const { Server } = require("ws");
-
-const wss = new Server({ server });
+let users = {};
 let labs = {};
-wss.on("connection", (client) => {
-  client.on("open", () => {});
-  client.on("message", function message(data) {
-    client.send("welcome to peerlab server");
-  });
-  client.on("error", (err) => {
-    client.close();
-  });
 
-  client.on("close", () => console.log("Client disconnected"));
+const wss = new WebSocketServer({ port: PORT });
+
+wss.on("connection", (ws) => {
+  ws.on("message", (data) => {});
+  ws.on("error", () => {});
+  ws.on("close", () => {});
 });
+
+// wss.on("connection", (client) => {
+//   client.on("open", () => {
+//     const userID = nanoid();
+//     users[userID] = client;
+//     console.log("hi");
+//   });
+//   client.on("message", (data) => {
+//     //     users[userId] = client; managing users
+//     const msg = JSON.parse(data.toString());
+
+//     if (msg.type === "NEW_USER") {
+//       newUser(client);
+//     }
+//     const isLab = labs.find(({ id }) => id === msg.id);
+//     if (!isLab) {
+//       labs.push(msg);
+//       // client.send("Room Created");
+//     } else {
+//       // client.send("room already exists");
+//     }
+
+//     console.log(isLab);
+
+//     // client.send("welcome to peerlab server");
+//   });
+//   client.on("error", (err) => {
+//     console.log(err);
+//     client.close();
+//   });
+
+//   client.on("close", () => {
+//     deleteUser(client);
+//   });
+// });
+
+const newUser = (client) => {
+  const newId = nanoid();
+  if (!users[newId]) {
+    let newClient = client;
+    newClient.id = newId;
+    users[newId] = newClient;
+    client.send(
+      JSON.stringify({
+        type: "ID_GENERATED",
+        body: { id: users[newId].id },
+      })
+    );
+  }
+  console.log(users);
+};
+const deleteUser = (client) => {
+  try {
+    delete users[client.userId];
+  } catch {}
+};
